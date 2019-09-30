f94e2e44abda7c3dd17acf567693945bcf9fce7d
Mirko Cukich <mirkocukich@gmail.com>
2019-09-29 22:48:12 -0500

    Program Complete

--- UIPickerView Demo/ViewController.swift
+++ UIPickerView Demo/ViewController.swift
@@ -13,27 +13,35 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var pickerView: UIPickerView!
     
-    var colorArray = []
+    var colorArray = ["White", "Yellow", "Orange", "Red", "Magenta", "Purple", "Blue", "Cyan", "Green",
+                        "Dark Green", "Brown", "Tan", "Light Grey", "Medium Grey", "Dark Grey", "Black"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        textLabel.text = colorArray[0]
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         
+        return 1
     }
 
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
+        return colorArray.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
+        return colorArray[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         
+        textLabel.text = colorArray[row]
     }
 
 }
